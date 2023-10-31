2ee0b613ca38ab4d196077f3f350173d3caf404f
LVQT <levietquocthinh@gmail.com>
2023-10-31 11:21:54 +0700

    js DOM

---
+++ JS/dom.js
@@ -0,0 +1,20 @@
+// adding an element 
+             const body = document.body
+            // body.append("hello world")
+// create an element
+// const div = document.createElement("div")
+//div.innerText ="dùng inner text "
+//div.textContent =" Dùng text content"
+// div.innerHTML="<strong>hello world</strong>"
+// body.append(div)
+
+// const div = document.querySelector("div")
+// console.log(div.textContent)
+// console.log(div.innerText)
+
+const div = document.querySelector("div")
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+// spanBye.remove()
+console.log (spanHi.getAttribute('id'))
