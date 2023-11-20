2558e60f9fd8d363279b9dcdd777949584c1e83d
LVQT <levietquocthinh@gmail.com>
2023-11-20 22:51:11 +0700

    js validate

--- JS/practice/animated_sidebar/validate/app.js
+++ JS/practice/animated_sidebar/validate/app.js
@@ -1,36 +1,73 @@
-var username = document.querySelector('#username')
-var email = document.querySelector('#email')
-var password = document.querySelector('#password')
-var confirmPassword = document.querySelector('#confirm-password')
-var form = document.querySelector('form')
-function showError(input,message){
-   let parent = input.parentElement;
+var username = document.querySelector('#username');
+var email = document.querySelector('#email');
+var password = document.querySelector('#password');
+var confirmPassword = document.querySelector('#confirm-password');
+var form = document.querySelector('form');
+
+function showError(input, message) {
+    let parent = input.parentElement;
     let small = parent.querySelector('small');
 
     parent.classList.add('error');
-    small.innerText = message
-    
+    small.innerText = message;
 }
-function showSuccess(input){
+
+function showSuccess(input) {
     let parent = input.parentElement;
-     let small = parent.querySelector('small');
- 
-     parent.classList.remove('error');
-     small.innerText = ''
-     
+    let small = parent.querySelector('small');
+
+    parent.classList.remove('error');
+    small.innerText = '';
 }
-function checkEmptyError(listInput){
+
+function checkEmptyError(listInput) {
+    let isEmptyError = false;
     listInput.forEach(input => {
-        input.value = input.value.trim()
-        if(!input.value){
-            showError(input,'Khong duoc de trong')
-        
-        }else {
-            showSuccess(input)
+        input.value = input.value.trim();
+
+        if (!input.value) {
+            isEmptyError = true;
+            showError(input, 'Không được để trống');
+        } else {
+            showSuccess(input);
         }
     });
+    return isEmptyError;
 }
-form.addEventListener('submit',function(e){
-    e.preventDefault()
-    checkEmptyError([useername,email,password,confirmPassword])
-})
+
+function checkEmailError(input) {
+    const regexEmail =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    input.value = input.value.trim();
+
+    let isEmailError = !regexEmail.test(input.value);
+
+    if (regexEmail.test(input.value)) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email không hợp lệ');
+    }
+    return isEmailError;
+}
+
+function checkLengthError(input,min,max){
+    input.value = input.value.trim()
+    if(input.value.length < min){
+        showError(input,`Phải có ít nhất ${min} ký tự`)
+        return true
+    }
+    if(input.value.length > max){
+        showError(input,`không được quá ${max} ký tự`)
+        return true
+    }
+    showSuccess(input)
+    return false
+}
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let isEmptyError = checkEmptyError([username, email, password, confirmPassword]);
+    let isEmailError = checkEmailError(email);
+    let isUsernameLengthError = checkEmailError(username);
+    let isPasswordLengthError = checkEmailError(password);
+});
